74b4b35d38de663a1724b8f292bc03b56ac75758
Nick Foden <nickfoden@gmail.com>
2018-01-20 13:59:22 -0500

    Stubbing some data for answers.

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,2 +1,14 @@
 import React from "react";
 import { Stylesheet, Text, View } from "react-native";
+
+class DeckList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the decklist component</Text>
+      </View>
+    );
+  }
+}
+
+export default DeckList;
---
+++ utils/api.js
@@ -0,0 +1,33 @@
+const initialData = {
+  Geography: {
+    title: "Geography",
+    questions: [
+      {
+        question: "Is South Africa a country?",
+        answer: "No, it is just a region",
+        correctAnswer: "false"
+      },
+      {
+        question: "Which US state is next to California?",
+        answer: "New York",
+        correctAnswer: "false"
+      }
+    ]
+  },
+  JavaScript: {
+    title: "Javascript",
+    questions: [
+      {
+        question: "What is a closure?",
+        answer:
+          "The combination of a function and the lexical envirom=nment within which that function was delcared",
+        correctAnswer: "true"
+      },
+      {
+        question: "What is a variable?",
+        answer: "Something that stores information",
+        correctAnswer: "true"
+      }
+    ]
+  }
+};
